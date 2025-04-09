52f9cf5bb591257003f8bdc5041d1a59065f73d6
rofiareiv <kambing.rofi@gmail.com>
2025-04-09 11:05:41 +0700

    Last update

--- adit/settings.py
+++ adit/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'adit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adit',
+        'USER': 'postgres',
+        'PASSWORD': 'rahasia',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
